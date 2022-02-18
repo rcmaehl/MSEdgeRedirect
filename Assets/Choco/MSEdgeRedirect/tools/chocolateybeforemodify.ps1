331de36b3a7b440d2baeb404dfb2b84d522c51c1
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2022-02-18 16:50:39 +0530

    Update chocolateybeforemodify.ps1

--- Assets/Choco/MSEdgeRedirect/tools/chocolateybeforemodify.ps1
+++ Assets/Choco/MSEdgeRedirect/tools/chocolateybeforemodify.ps1
@@ -6,4 +6,4 @@
 #  uninstall script so that it doesn't fail when it is already uninstalled.
 # NOTE: For upgrades - like the uninstall script, this script always runs from 
 #  the currently installed version, not from the new upgraded package version.
-
+#
