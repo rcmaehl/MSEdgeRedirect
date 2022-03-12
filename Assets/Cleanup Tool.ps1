1682dde7b8281217835d5f7747d124db10ee9163
Robert C. Maehl <rcmaehl@aol.com>
2022-03-11 19:35:16 -0500

    Don't display errors if files don't exist

--- Assets/Cleanup Tool.ps1
+++ Assets/Cleanup Tool.ps1
@@ -3,30 +3,30 @@
 # Remove Files
 
 ## Active Mode Install
-Remove-Item -Path "C:\Program Files\MSEdgeRedirect\*" -Force
-Remove-Item -Path "C:\Program Files\MSEdgeRedirect" -Force
-Remove-Item -Path "C:\Program Files\Microsoft\Edge\Application\msedge_no_ifeo.exe" -Force
-Remove-Item -Path "C:\Program Files (x86)\Microsoft\Edge\Application\msedge_no_ifeo.exe" -Force
+Remove-Item -Path "C:\Program Files\MSEdgeRedirect\*" -Force -ErrorAction "Ignore"
+Remove-Item -Path "C:\Program Files\MSEdgeRedirect" -Force -ErrorAction "Ignore"
+Remove-Item -Path "C:\Program Files\Microsoft\Edge\Application\msedge_no_ifeo.exe" -Force -ErrorAction "Ignore"
+Remove-Item -Path "C:\Program Files (x86)\Microsoft\Edge\Application\msedge_no_ifeo.exe" -Force -ErrorAction "Ignore"
 
 ## Service Mode Install
-Remove-Item -Path $env:LOCALAPPDATA\MSEdgeRedirect\* -Force
-Remove-Item -Path $env:LOCALAPPDATA\MSEdgeRedirect -Force
+Remove-Item -Path $env:LOCALAPPDATA\MSEdgeRedirect\* -Force -ErrorAction "Ignore"
+Remove-Item -Path $env:LOCALAPPDATA\MSEdgeRedirect -Force -ErrorAction "Ignore"
 
 ## Start Menu Items
-Remove-Item -Path $env:APPDATA\Microsoft\Windows\Start Menu\Programs\MSEdgeRedirect\* -Force
-Remove-Item -Path $env:APPDATA\Microsoft\Windows\Start Menu\Programs\MSEdgeRedirect -Force
+Remove-Item -Path $env:APPDATA\Microsoft\Windows\Start Menu\Programs\MSEdgeRedirect\* -Force -ErrorAction "Ignore"
+Remove-Item -Path $env:APPDATA\Microsoft\Windows\Start Menu\Programs\MSEdgeRedirect -Force -ErrorAction "Ignore"
 
 # Remove Registry
 
 ## Active Mode Install
-Remove-Item "HKLM:\Software\Classes\MSEdgeRedirect" -Recurse -Force
-Remove-Item "HKLM:\Software\Classes\Applications\MSEdgeRedirect.exe" -Recurse -Force
-Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MSEdgeRedirect" -Recurse -Force
-Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\MSEdgeRedirect.exe" -Recurse -Force
-Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" -Recurse -Force
+Remove-Item "HKLM:\Software\Classes\MSEdgeRedirect" -Recurse -Force -ErrorAction "Ignore"
+Remove-Item "HKLM:\Software\Classes\Applications\MSEdgeRedirect.exe" -Recurse -Force -ErrorAction "Ignore"
+Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MSEdgeRedirect" -Recurse -Force -ErrorAction "Ignore"
+Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\MSEdgeRedirect.exe" -Recurse -Force -ErrorAction "Ignore"
+Remove-Item "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\msedge.exe" -Recurse -Force -ErrorAction "Ignore"
 
 ## Service Mode Install
-Remove-Item "HKCU:\Software\Classes\MSEdgeRedirect" -Recurse -Force
-Remove-Item "HKCU:\Software\Classes\Applications\MSEdgeRedirect.exe" -Recurse -Force
-Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MSEdgeRedirect" -Recurse -Force
-Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\MSEdgeRedirect.exe" -Recurse -Force
+Remove-Item "HKCU:\Software\Classes\MSEdgeRedirect" -Recurse -Force -ErrorAction "Ignore"
+Remove-Item "HKCU:\Software\Classes\Applications\MSEdgeRedirect.exe" -Recurse -Force -ErrorAction "Ignore"
+Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MSEdgeRedirect" -Recurse -Force -ErrorAction "Ignore"
+Remove-Item "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\MSEdgeRedirect.exe" -Recurse -Force -ErrorAction "Ignore"
