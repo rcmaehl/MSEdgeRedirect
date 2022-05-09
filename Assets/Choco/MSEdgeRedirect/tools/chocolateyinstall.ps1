bb14f1de50db8d3b1e33ef0b4bea23676e280e3c
Naveen Prashanth <78990165+gnpaone@users.noreply.github.com>
2022-05-09 22:35:20 +0530

    Update chocolateyinstall.ps1

--- Assets/Choco/MSEdgeRedirect/tools/chocolateyinstall.ps1
+++ Assets/Choco/MSEdgeRedirect/tools/chocolateyinstall.ps1
@@ -1,9 +1,6 @@
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
   fileType       = 'exe'
-  url            = '{URL32}'
-  checksum       = '{SHA256CHECKSUM32}'
-  checksumType   = 'sha256'
   url64Bit       = '{URL64}'
   checksum64     = '{SHA256CHECKSUM64}'
   checksumType64 = 'sha256'
